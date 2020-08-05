a3cf4ac7fcf3bd71871d6ffc797dec585a76b7c0
Natalia <nataliamirandac@hotmail.com>
2020-08-05 14:44:32 -0400

    Lesson's notes

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const server = express();
 
-server.use(express.json());
+server.use(express.json()); //built-in middleware - no need to npm install it
+server.use(morgan("dev"));
+server.use("/api/hubs", morgan("short"), hubsRouter);
 
-server.use('/api/hubs', hubsRouter);
+server.use(helmet()); //free extra security. always use it.
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.use(logger);
 
-  res.send(`
-    <h2>Lambda Hubs API</h2>
-    <p>Welcome${nameInsert} to the Lambda Hubs API</p>
-    `);
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: req.name });
+  // const nameInsert = req.name ? ` ${req.name}` : "";
+
+  // // res.send(`
+  // //   <h2>Lambda Hubs API</h2>
+  // //   <p>Welcome${nameInsert} to the Lambda Hubs API</p>
+  // //   `);
 });
+
+//req, res, next
+function logger(req, res, next) {
+  const name = req.headers.name;
+  req.name = name;
+  console.log(`a ${req.name} made a ${req.method} request to ${req.url}`);
+  next();
+}
 
 module.exports = server;
